0fd2302600a7a3f17c3b5d14e1bf6840c0081ea7
Roesn <Bubwub04@icloud.com>
2021-10-13 14:57:35 -0400

    ex41skele

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,42 @@
+package baseline;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution41 {
+
+    // main
+    public static void main(String[] args)throws FileNotFoundException {
+
+        // create Solution41 class variable
+
+        // create a scanner to read from input file
+
+        // declare an ArrayList for Strings to store the input names from the file
+
+        // read the names from the input file with readNamesFromInput
+
+        // sort the list of names in alphabetical order using sort function
+
+        // close the scanner after reading the entire input file
+
+        // write the data to the output file
+        // print any exception that may happen when writing to output file
+    }
+
+    // for writing sorted names to output
+    public void outputNames(ArrayList<String> names) throws IOException {
+
+        // create a file writer for output file
+
+        // write the number of names to the output file and put a line spacer
+
+        // write the names in the file
+
+        // close output file
+    }
+}
---
+++ exercise41/src/main/java/baseline/sortAllNames.java
@@ -0,0 +1,15 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class sortAllNames {
+
+        // define function to scan the data from Scanner to the ArrayList
+        public static void readNamesFromInput(Scanner in, ArrayList<String> names) {
+
+            // read the data till the file is empty
+
+            // add the name to the list of names
+        }
+}
