36caaeb432069bd9ef9e6da99ffa2e79143a04c1
Roesn <Bubwub04@icloud.com>
2021-10-17 23:30:43 -0400

    ex45skele

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,97 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+
+package baseline;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution45 {
+
+    // static scanner
+
+    // input method
+    private String inputMethod() {
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        // make Solution45 class variable
+
+        // prompt for output file
+
+        // make readfile object
+
+        // read file
+
+        // get temp list from input
+
+        // pass output file and list to write file for output
+
+        // write output
+    }
+}
+
+
+// class for ReadFile
+class ReadFile{
+
+    // declare variables
+
+    // constructor
+    public ReadFile() throws FileNotFoundException {
+
+        // declare list for input file
+
+        // make file to input file
+
+        // make file scanner
+    }
+
+    public void readFile() {
+
+        // error handling
+        try {
+
+            // scan input file and store in temp array
+            while( ) {
+
+            }
+        } // when done close scanner object
+        finally {
+        }
+    }
+
+    //return the temp list
+    public ArrayList<String> getUtilize() {
+    }
+}
+
+// write to output class
+class WriteFile{
+
+    // declare variables
+
+    // method for writing to output file
+    public WriteFile(String name, ArrayList<String> list) throws IOException {
+
+        // initialize variables
+    }
+
+    // print to output file
+    public void printToOutputFile() throws IOException {
+
+        // loop through till end of input file
+        for () {
+
+            // replace utilize with use
+
+            // write to output file
+        }
+
+        // close the writer object
+    }
+}
