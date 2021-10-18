feec1686194cbb4a7a6a499158811ba23aefc5d8
Roesn <Bubwub04@icloud.com>
2021-10-18 00:00:16 -0400

    ex46skele

---
+++ exercise46/src/main/java/baseline/Solution46.java
@@ -0,0 +1,114 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Trae Roy
+ */
+
+package baseline;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Solution46 {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // create an object of ReadInputFile file
+
+        //read file
+
+        // get map
+
+        // create an object of PrintReport class and
+
+        // print it on screen
+    }
+}
+
+// class to read file
+class ReadInputFile{
+
+    // declare variables
+
+    // method to read input file
+    public ReadInputFile() throws FileNotFoundException {
+        // file for input file
+    }
+
+    // read file
+    public void readFile() {
+
+        // error handling
+
+            // loop while not null
+
+                // read input whole line
+
+                // split it by spaces
+
+                // for loop to loop thorugh input file
+
+                    // if key is already in map then
+
+                        // update value
+
+                    // else put it in the map
+
+
+        // at end close scanner object
+
+
+
+
+    // method for getting map
+    public HashMap<String, Integer> getMap(){
+
+    }
+}
+
+// class to print output
+class PrintOutput{
+
+    // constant map
+
+    // print output method
+    public PrintOutput( HashMap<String, Integer> map) {
+        // initialize constructor
+
+    }
+
+    // method for print histogram on screen
+    public void printScreen() {
+
+        // declare key and value array
+
+        // initialize k index variable
+
+        // iterate map
+
+        // store key and value in respective arrays
+
+        // declare another arrays of String and Integer to Store values
+
+        // copy temp values array to values
+
+        // reverse sort values
+
+        // sort keys according to values array
+
+        // initialize value array at index
+
+
+        // store keys in temp keys array
+
+        // print new line
+
+        // print histogram
+
+
+        // print '*'
+
+        // print new line
+
+        }
+    }
+}
