e9459278771e32cecd55d1aa9b102dbfe7550f5e
Roesn <Bubwub04@icloud.com>
2021-10-17 21:22:30 -0400

    ex42psuedoandinput

---
+++ exercise42/src/main/java/baseline/Solution42.java
@@ -0,0 +1,48 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Trae Roy
+ */
+
+
+package baseline;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Solution42 {
+
+    // constant scanner for String input
+
+    // input method for salary
+    private String inputMethod() {
+    }
+
+    // main
+    public static void main(String[] args) {
+
+        // class variable sol
+
+        // print header
+
+        // print line spacer
+
+        // print to output file using printOutput
+    }
+
+    // output method
+    public void printOutput(String filename) {
+        // try to read input file
+        try {
+
+            // continue while there's a name on next line, parse using regex
+            while (in.hasNext()) {
+
+            }
+
+            // else no file print exception
+        } catch (FileNotFoundException ex) {
+
+        }
+    }
+}
