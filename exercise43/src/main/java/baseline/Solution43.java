2b2fc9f9b3da458780a9e0efd6c868c1fb65d724
Roesn <Bubwub04@icloud.com>
2021-10-17 22:24:21 -0400

    ex43psuedo

---
+++ exercise43/src/main/java/baseline/Solution43.java
@@ -0,0 +1,81 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Trae Roy
+ */
+
+package baseline;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Solution43 {
+
+    // static scanner
+
+    // main
+    public static void main(String[] args) {
+
+        // declare variables
+
+        // prompt for website name
+
+        // prompt for author
+
+        // prompt for javascript folder
+
+        // prompt for css folder
+
+        // get path
+
+        // add site name to path
+
+        // make a new file object for path
+
+        // make directory for new website
+
+        // if website created
+
+            // make sub directories
+
+            // try to create index.HTML website
+
+                // create file write object
+
+                // write data to website
+
+                // close makeWebsite
+
+                // print html created
+
+                // if javaScriptCreated is yes then make javaScript folder
+
+                    // make javaScript file object
+
+                    // make sure javaScript folder is made correctly
+
+                    // print javaScript folder created
+
+                  // else print javaScript folder not created
+
+
+                // if css is yes then make css folder
+
+                    // make css file object
+
+                    // make sure css folder is made correctly
+
+                    // print css folder created
+
+
+                     // else print css folder not created
+
+
+            // throw exception if file is empty
+
+         // print exception message because website not created
+
+        // close scanner
+    }
+}
