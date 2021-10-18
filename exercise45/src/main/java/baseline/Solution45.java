2b32dd98d49f7726f0058964c7450c003234fda8
Roesn <Bubwub04@icloud.com>
2021-10-17 23:36:57 -0400

    ex45umlandjava

--- exercise45/src/main/java/baseline/Solution45.java
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -12,26 +12,36 @@ import java.util.Scanner;
 public class Solution45 {
 
     // static scanner
+    private static final Scanner input = new Scanner(System.in);
 
     // input method
     private String inputMethod() {
+        System.out.println("Enter the output file : ");
+        return input.nextLine();
     }
 
     public static void main(String[] args) throws IOException {
 
         // make Solution45 class variable
+        Solution45 sol = new Solution45();
 
         // prompt for output file
+        String output = sol.inputMethod();
 
         // make readfile object
+        ReadFile read = new ReadFile();
 
         // read file
+        read.readFile();
 
         // get temp list from input
+        ArrayList<String> list = read.getUtilize();
 
         // pass output file and list to write file for output
+        WriteFile write = new WriteFile(output, list);
 
         // write output
+        write.printToOutputFile();
     }
 }
 
@@ -40,15 +50,20 @@ public class Solution45 {
 class ReadFile{
 
     // declare variables
+    private final ArrayList<String> temp;
+    private final Scanner input;
 
     // constructor
     public ReadFile() throws FileNotFoundException {
 
         // declare list for input file
+        temp = new ArrayList<>();
 
         // make file to input file
+        File file = new File("./data/exercise45_input.txt");
 
         // make file scanner
+        input = new Scanner(file);
     }
 
     public void readFile() {
@@ -57,16 +72,21 @@ class ReadFile{
         try {
 
             // scan input file and store in temp array
-            while( ) {
+            while( input.hasNext() ) {
 
+                String data = input.nextLine();
+                temp.add(data);
             }
         } // when done close scanner object
         finally {
+            input.close();
         }
     }
 
     //return the temp list
     public ArrayList<String> getUtilize() {
+
+        return temp;
     }
 }
 
@@ -74,24 +94,31 @@ class ReadFile{
 class WriteFile{
 
     // declare variables
+    private final FileWriter writer;
+    private final ArrayList<String> list;
 
     // method for writing to output file
     public WriteFile(String name, ArrayList<String> list) throws IOException {
 
         // initialize variables
+        writer = new FileWriter("./data/" + name);
+        this.list = list;
     }
 
     // print to output file
     public void printToOutputFile() throws IOException {
 
         // loop through till end of input file
-        for () {
+        for (String temp : list) {
 
             // replace utilize with use
+            temp = temp.replaceAll("utilize", "use");
 
             // write to output file
+            writer.write(temp + "\n");
         }
 
         // close the writer object
+        writer.close();
     }
 }
